d7c37bf1d75777e8a9cc6ee13dcc05cefe38eaa0
Gary Ascuy <gary.ascuy@gmail.com>
2020-04-26 20:22:34 -0500

    Adding base Express API

---
+++ api/readme.md
@@ -0,0 +1,13 @@
+# Continuous Integration as Code - Backend (API)
+
+### Development Setup
+
+`yarn` is required for this project, you can install using `npm i -g yarn`.
+
+```sh
+$ yarn install
+```
+
+```sh
+$ yarn start
+```
--- api/src/app.js
+++ api/src/app.js
@@ -1,8 +1,15 @@
 import { get } from './settings'
+import express from 'express'
 
 export async function main() {
-  console.log(get('server'))
-  
+  const { host, port } = get('server')
+  const app = express();
+
+  app.get('/', (req, res) => {
+      res.send('An alligator approaches!');
+  })
+
+  app.listen(+port, host, () => console.log('API listening on port 3000!'))
 }
 
 main()
