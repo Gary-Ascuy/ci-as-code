1c477d3e98fbee234b9cf314509ca28427d24d85
Gary Ascuy <gary.ascuy@gmail.com>
2020-04-28 00:21:38 -0500

    Adding about page

--- ui/src/components/Gary/Gary.js
+++ ui/src/components/Gary/Gary.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import { Image, Segment } from 'semantic-ui-react'
 
 export default function Gary() {
   return (
-    <div>Gary Ascuy</div>
+    <>
+      <Image src='/profile/gary.ascuy.jpg' size='medium' circular centered />
+      <Segment style={{ fontSize: '15px', textAlign: 'center', fontStyle: 'italic' }} padded basic>Gary Ascuy is a Senior Software Developer with 8+ years of experience, he likes Robotics, Electronic and Informatics, and he loves to play with electronics in his free time and he is part of Docker Cochabamba Community, he also has a lot of experience putting small/medium systems in production.</Segment>
+      <Segment style={{ fontSize: '25px', textAlign: 'center', fontStyle: 'italic' }} padded basic>Copyright © 2020</Segment>
+    </>
   )
 }
