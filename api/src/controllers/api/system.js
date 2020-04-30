2de0c82e10d772aa4f5b93d39ad77e6d293131d4
Gary Ascuy <gary.ascuy@gmail.com>
2020-04-29 21:18:55 -0500

    Updating CI for API

--- api/src/controllers/api/system.js
+++ api/src/controllers/api/system.js
@@ -1,6 +1,6 @@
 import os from 'os'
 import express from 'express'
-import { omit } from 'lodash'
+import _ from 'lodash'
 
 import { data } from '../helpers'
 import { logger } from '../../logger'
@@ -9,7 +9,7 @@ const osInfo = {
   version: os.version(),
   hostname: os.hostname(),
   homedir: os.homedir(),
-  cpus: os.cpus().map((cpu) => omit(cpu, 'times')),
+  cpus: os.cpus().map((cpu) => _.omit(cpu, 'times')),
 }
 
 export const system = express.Router()
